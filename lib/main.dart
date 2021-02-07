6cdfb71e6ff76d101f77a3cdbc1cfd278cff72a8
slowy07 <slowy.arfy@gmail.com>
2021-02-08 03:29:05 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:instagram_ui_redesign/screens/feed_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
@@ -9,6 +8,7 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       title: 'Flutter instagram ui redesign',
       debugShowCheckedModeBanner: false,
+      home: FeedScreen(),
     );
   }
 }
